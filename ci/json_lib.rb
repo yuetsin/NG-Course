f8e3d4aef2a84039c91acc38dde0f8e042b19988
yuetsin <akaza_akari@sjtu.edu.cn>
2019-09-16 23:13:24 +0800

    [fix] change json_lib.rb

--- ci/json_lib.rb
+++ ci/json_lib.rb
@@ -3,13 +3,5 @@
 module JsonLib
     class << self; attr_accessor :json_files; end
     self.json_files = [
-        "./release/2016_2017_1.json", 
-        "./release/2016_2017_2.json", 
-        "./release/2016_2017_3.json", 
-        "./release/2017_2018_1.json", 
-        "./release/2017_2018_2.json", 
-        "./release/2017_2018_3.json", 
-        "./release/2018_2019_1.json", 
-        "./release/2018_2019_2.json",
-        "./release/2018_2019_3.json"];
+        "./release/2019_2020/1.json"];
 end
