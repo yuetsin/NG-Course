8bc61fdcd5da0c7b9af4e869a253683939a40b57
yuetsin <akaza_akari@sjtu.edu.cn>
2019-09-16 19:58:48 +0800

    [fix] ci relative path

--- ci/json_lib.rb
+++ ci/json_lib.rb
@@ -3,13 +3,13 @@
 module JsonLib
     class << self; attr_accessor :json_files; end
     self.json_files = [
-        "./json_output/2016_2017_1.json", 
-        "./json_output/2016_2017_2.json", 
-        "./json_output/2016_2017_3.json", 
-        "./json_output/2017_2018_1.json", 
-        "./json_output/2017_2018_2.json", 
-        "./json_output/2017_2018_3.json", 
-        "./json_output/2018_2019_1.json", 
-        "./json_output/2018_2019_2.json",
-        "./json_output/2018_2019_3.json"];
+        "./release/2016_2017_1.json", 
+        "./release/2016_2017_2.json", 
+        "./release/2016_2017_3.json", 
+        "./release/2017_2018_1.json", 
+        "./release/2017_2018_2.json", 
+        "./release/2017_2018_3.json", 
+        "./release/2018_2019_1.json", 
+        "./release/2018_2019_2.json",
+        "./release/2018_2019_3.json"];
 end
