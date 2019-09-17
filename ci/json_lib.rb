0b6ed400f0d9c62d9653f6c7c38f20f9a2d7f310
yuetsin <akaza_akari@sjtu.edu.cn>
2019-09-17 09:55:13 +0800

    [fix] JsonLib path

--- ci/json_lib.rb
+++ ci/json_lib.rb
@@ -3,5 +3,8 @@
 module JsonLib
     class << self; attr_accessor :json_files; end
     self.json_files = [
-        "./release/2019_2020/1.json"];
+        "./release/2018_2019_1.json",
+        "./release/2018_2019_2.json",
+        "./release_2018_2019_3.json",
+        "./release/2019_2020_1.json"];
 end
