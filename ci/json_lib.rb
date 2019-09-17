4f836f7b68a25566d65c06f919fcf1785f1326bb
yuetsin <akaza_akari@sjtu.edu.cn>
2019-09-17 15:52:25 +0800

    [fix] remove legacy files

--- ci/json_lib.rb
+++ ci/json_lib.rb
@@ -3,9 +3,6 @@
 module JsonLib
     class << self; attr_accessor :json_files; end
     self.json_files = [
-        "./release/2017_2018_1.json",
-        "./release/2017_2018_2.json",
-        "./release/2017_2018_3.json",
         "./release/2018_2019_1.json",
         "./release/2018_2019_2.json",
         "./release/2018_2019_3.json",
