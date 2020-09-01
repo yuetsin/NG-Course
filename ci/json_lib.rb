b0d5e4be50e92101895c6ffa36cab0cd3464c968
yuetsin <akaza_akari@sjtu.edu.cn>
2020-09-01 11:37:46 +0800

    [add] 2020_2021_1 under graduate course info

--- ci/json_lib.rb
+++ ci/json_lib.rb
@@ -9,7 +9,7 @@ module JsonLib
         "./release/2019_2020_1.json",
         "./release/2019_2020_2.json",
         "./release/2019_2020_3.json",
-        # "./release/2020_2021_1.json",
+        "./release/2020_2021_1.json",
         "./example/10.json",
         "./example/100.json",
         "./example/1000.json"];
